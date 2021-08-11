eaa1228afa985f2fdbbc2421dff2659e8a5843c9
Nick Zana <me@nickzana.dev>
2021-08-11 08:30:17 +0000

    Rewrite build script
    Remove unecessary complexity from build script
    Remove Language type
    Implement template system for iso639_enum.rs
    Eliminate many unecessary memory allocations
    Implement Display for Err
    Update tests to reflect new Result<Iso639, Err> return type

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,7 @@ mod iso_639;
 mod iso_compat;
 
 pub use iso_639::Iso639;
-pub use iso_639::Iso639::*;
-pub use iso_compat::IsoCompat;
+pub use iso_compat::*;
 
 #[cfg(test)]
 mod tests {
@@ -22,9 +21,9 @@ mod tests {
 
     #[test]
     fn test_get_from_properties() {
-        assert_eq!(Iso639::from_name("English"), Some(Iso639::Eng));
-        assert_eq!(Iso639::from_iso639_3("eng"), Some(Iso639::Eng));
-        assert_eq!(Iso639::from_iso639_1("en"), Some(Iso639::Eng));
-        assert_eq!(Iso639::from_autonym("English"), Some(Iso639::Eng));
+        assert_eq!(Iso639::from_name("English").ok(), Some(Iso639::Eng));
+        assert_eq!(Iso639::from_iso639_3("eng").ok(), Some(Iso639::Eng));
+        assert_eq!(Iso639::from_iso639_1("en").ok(), Some(Iso639::Eng));
+        assert_eq!(Iso639::from_autonym("English").ok(), Some(Iso639::Eng));
     }
 }
