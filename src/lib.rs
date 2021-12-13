194e63d693134447f2410da02ba0b5e08a351854
Nick Zana <me@nickzana.dev>
2021-12-12 19:25:02 -0500

    Fix breaking API change of IsoCompat scope
    Make iso_compat::IsoComat trait public
    Update tests module to use publicly exposed API
    Bump version to 0.6

--- src/lib.rs
+++ src/lib.rs
@@ -5,27 +5,26 @@ mod iso_compat;
 mod localized_string;
 
 pub use iso_639::Iso639 as Language;
-pub use iso_compat::Err;
+pub use iso_compat::{Err, IsoCompat};
 pub use localized_string::*;
 
 #[cfg(test)]
 mod tests {
-    use crate::iso_639::Iso639;
-    use crate::iso_compat::IsoCompat;
+    use super::{IsoCompat, Language};
 
     #[test]
     fn test_get_properties() {
-        assert_eq!(Iso639::Eng.name(), "English");
-        assert_eq!(Iso639::Eng.iso639_3(), "eng");
-        assert_eq!(Iso639::Eng.iso639_1(), Some("en"));
-        assert_eq!(Iso639::Eng.autonym(), Some("English"));
+        assert_eq!(Language::Eng.name(), "English");
+        assert_eq!(Language::Eng.iso639_3(), "eng");
+        assert_eq!(Language::Eng.iso639_1(), Some("en"));
+        assert_eq!(Language::Eng.autonym(), Some("English"));
     }
 
     #[test]
     fn test_get_from_properties() {
-        assert_eq!(Iso639::from_name("English").ok(), Some(Iso639::Eng));
-        assert_eq!(Iso639::from_iso639_3("eng").ok(), Some(Iso639::Eng));
-        assert_eq!(Iso639::from_iso639_1("en").ok(), Some(Iso639::Eng));
-        assert_eq!(Iso639::from_autonym("English").ok(), Some(Iso639::Eng));
+        assert_eq!(Language::from_name("English").ok(), Some(Language::Eng));
+        assert_eq!(Language::from_iso639_3("eng").ok(), Some(Language::Eng));
+        assert_eq!(Language::from_iso639_1("en").ok(), Some(Language::Eng));
+        assert_eq!(Language::from_autonym("English").ok(), Some(Language::Eng));
     }
 }
